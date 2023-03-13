dbda287bec956ab8fbe998d2b3849172d2ddccd6
Xuanyu Zhang <126142147+LouisanaZhangXuanyu@users.noreply.github.com>
2023-03-13 19:18:33 +0800

    Comitted Exercise4

--- src/test/java/prog2/exercise4/flight/booking/system/FlightBookingTest.java
+++ src/test/java/prog2/exercise4/flight/booking/system/FlightBookingTest.java
@@ -3,29 +3,30 @@ package prog2.exercise4.flight.booking.system;
 import static org.junit.Assert.assertEquals;
 import java.time.LocalDate;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
-public class FlightBookingTest 
+public class FlightBookingTest
 {
-    
+
     @Test
     public void testing_Setting_Up_Trip_Source_Using_Enum() {
         FlightBooking.TripSource tripSource = FlightBooking.TripSource.NANJING;
-        assertEquals(FlightBooking.TripSource.valueOf("NANJING"), tripSource);
+        Assert.assertEquals(FlightBooking.TripSource.valueOf("NANJING"), tripSource);
     }
 
     @Test
     public void testing_Setting_Up_Booking_Class_Using_Enum() {
         FlightBooking.BookingClass bookingClass = FlightBooking.BookingClass.FIRST;
-        assertEquals(FlightBooking.BookingClass.valueOf("FIRST"), bookingClass);
+        Assert.assertEquals(FlightBooking.BookingClass.valueOf("FIRST"), bookingClass);
     }
 
     @Test
     public void testing_total_values_in_Booking_Class_Enum() {
         int expected = 3;
         int result = FlightBooking.BookingClass.values().length;
-        assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -33,15 +34,15 @@ public class FlightBookingTest
         FlightBooking fb = new FlightBooking(null, null, null, 0, 0);
         fb.setTripSource("2");
         FlightBooking.TripSource expected = FlightBooking.TripSource.BEIJING;
-        FlightBooking.TripSource  result = fb.getTripSource(); 
-        assertEquals(expected, result);
+        FlightBooking.TripSource  result = fb.getTripSource();
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testing_Departure_And_Return_Date_Difference() {
-        
+
         FlightBooking fb = new FlightBooking(null, null, null, 0, 0);
-        
+
         String sdepart = "2023-03-04";
         LocalDate departureDate = LocalDate.parse(sdepart);
 
@@ -55,18 +56,18 @@ public class FlightBookingTest
 
         fb.setReturnDate(returnDate);
         LocalDate result = fb.getReturnDate();
-        
+
         //int result = returningOn.compareTo(departingOn);
 
         //assertNotEquals(expected, result);
-        assertEquals(expected, result);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testing_Conditional_Ticket_Number_Generation() {
-        
+
         FlightBooking fb = new FlightBooking(null, null, null, 0, 0);
-        
+
         FlightBooking.TripSource source = FlightBooking.TripSource.NANJING;
         FlightBooking.TripDestination destination = FlightBooking.TripDestination.BEIJING;
 
@@ -79,7 +80,7 @@ public class FlightBookingTest
 
         fb.setTripType("1");
         fb.setBookingClass("1");
-        
+
         String expectedTicketNumber = "11FASDFDOM";
 
         String expectedSubstring1 = expectedTicketNumber.substring(0,2);
@@ -88,21 +89,21 @@ public class FlightBookingTest
         String ticketNumber = fb.getTicketNumber();
 
         String returnedSubstring1 = ticketNumber.substring(0, 2);
-        
+
         String returnedSubstring2 = ticketNumber.substring(7, 9);
 
         if(expectedSubstring1.equals(returnedSubstring1) && expectedSubstring2.equals(returnedSubstring2)){
-            assertEquals(1,1);
+            Assert.assertEquals(1,1);
         }
         else {
-            assertEquals(1,2);
+            Assert.assertEquals(1,2);
         }
     }
 
     @Test
     public void testing_Conditional_Ticket_Price_Calculation() {
         FlightBooking fb = new FlightBooking(null, null, null, 0, 0);
-        
+
         FlightBooking.TripSource source = FlightBooking.TripSource.NANJING;
         FlightBooking.TripDestination destination = FlightBooking.TripDestination.BEIJING;
 
@@ -127,10 +128,10 @@ public class FlightBookingTest
         double expectedPrice = Math.abs((((child *((300 + (0.1*300)) + (0.05*300))) + (adults*((300 + (0.1*300)) + (0.05*300)))) + 250)*2);
 
         if (returnedPrice == expectedPrice){
-            assertEquals(1, 1);
+            Assert.assertEquals(1, 1);
         }
         else {
-            assertEquals(1, 2);
+            Assert.assertEquals(1, 2);
         }
-    }   
+    }
 }
